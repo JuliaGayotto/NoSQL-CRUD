fb51e6f8081d33826d1cdf98454595a83c062d81
JuliaGayotto <juliagayotto@gmail.com>
2023-05-28 10:14:10 -0300

    Adicao da funcao selecionarCompra

--- src/compra.py
+++ src/compra.py
@@ -12,11 +12,11 @@ global mydb
 mydb = client.mercadoLivre
 
 def menuCompra():
-    print("\nCOMPRA")
+    print("\nCOMPRAS")
     print("Escolha uma ação \n")
-    print(" 1 - Adicionar compras \n 2 - Visualizar todas as compras \n 3 - Visualizar compra \n 4 - Atualizar compra \n 5 - Deletar compra \n 0 - Voltar ao menu \n")
+    print(" 1 - Adicionar compra \n 2 - Visualizar todas as compras \n 3 - Visualizar compra \n 4 - Deletar compra \n 0 - Voltar ao menu \n")
     acao = int(input("Escolha uma acao: "))
-    opcoes = [0,1,2,3,4,5]
+    opcoes = [0,1,2,3,4]
     while acao not in opcoes:
         acao = int(input("Digite o número de uma ação válida: "))
 
@@ -28,21 +28,13 @@ def menuCompra():
         print("\nCOMPRAS")
         compras = visualizarCompras()
         for compra in compras:
-            print(f"\nData da Compra: {compra.get('data_compra')} \nUsuario: {compra.get('usuario')} \nVendedor: {compra.get('vendedor')} \nProduto: {compra.get('produtos')}") 
-    
+            selecionarCompra(compra)    
     elif acao == 3:
         id = input("Digite o id da compra que deseja visualizar: ")
         compra = visualizarCompra(id)
-        print(type(compra))
         print("\nCOMPRA ESCOLHIDA")
-        print(f"\nData da Compra: {compra.get('data_compra')} \nUsuario: {compra.get('usuario')} \nVendedor: {compra.get('vendedor')} \nProduto: {compra.get('produtos')}") 
+        selecionarCompra(compra) 
     elif  acao == 4:
-        print("\nATUALIZAR \nCOMPRAS:")
-        id = input("Digite o id da compra que deseja atualizar: ")
-        atualizarCompra(id)
-        print(f'\nCompra atualizado com sucesso!')
-    
-    elif  acao == 5:
         print("\nDELETAR")
         id = input("Digite o id da compra que deseja deletar: ")
         deletarCompra(id)
@@ -52,20 +44,30 @@ def menuCompra():
         menuCompra()
 
 
-def listarCompras():
-    print(f"\nId: {produto.get('nome')} \nPreço: {produto.get('preco')} \nQuantidade: {produto.get('quant_produto')}")
+def selecionarCompra(compra):
+    print(f"\nId da compra: {compra.get('_id')}")
+    print(f"Data da Compra: {compra.get('data_compra')}")
+    usuario = compra.get("usuario")
+    print(f"Usuário:  Nome: {usuario.get('nome')}  Email: {usuario.get('email')}  CPF: {usuario.get('cpf')}")
+    vendedor = compra.get("vendedor")
+    print(f"Vendedor:  Nome: {vendedor.get('nome_vendedor')}  Email: {vendedor.get('email')}  CPF: {vendedor.get('cpf')}")
+    produtos = compra.get('produtos')
+    for produto in produtos:
+        print(f"Produto:  Nome: {produto.get('nome')}  Preço: {produto.get('preco')}  Quantidade comprada: {produto.get('quant_comprada')}")
     
+
 def inserirCompra():
     global mydb
     dataCompra = input("Digite a data da compra: ")
     produtos = []
+    desejo  = 'S'
     while desejo != "N":
         nomeProduto = input("Digite o nome do produto: ")
         quant = int(input("Digite a quantidade comprada: "))
         prod = visualizarProduto(nomeProduto)
         produto = {"nome": prod.get('nome'), "preco": prod.get('preco'), "quant_comprada": quant }
         produtos.append(produto)
-        desejo = input("Deseja adicionar outro produto à compra? S/N")
+        desejo = input("Deseja adicionar outro produto à compra? S/N ")
     emailVendedor = input("Digite o email do vendedor: ")
     vend = visualizarVendedor(emailVendedor)
     vendedor = {"nome_vendedor": vend.get('nome_vendedor'), "email": vend.get('email'), "cpf": vend.get('cpf')}
@@ -90,29 +92,9 @@ def visualizarCompra(id):
     return mycol.find_one(myquery)
 
 
-def atualizarProduto():
-    global mydb
-    nome = input("Digite o id da compra cadastrada que deseja atualizar: ")
-    myquery = visualizarCompra(id)
-    mycol = mydb.compra
-    novoNome = input("Digite o nome do produto: ")
-    novoPreco = float(input("Digite o preço: "))
-    novaQuantidade = int(input("Digite a quantidade do produto: "))
-    novoProduto = {"nome": novoNome, "preco": novoPreco, "quant_produto": novaQuantidade}
-    novoNome = input("Digite o nome completo do vendedor: ")
-    novoEmail = input("Digite o email: ")
-    novoCpf = input("Digite o cpf: ")
-    novoVendedor = {"nome": novoNome, "email": novoEmail, "cpf": novoCpf}
-    novoPreco = float(input("Digite o preço: "))
-    novaQuant_produto = int(input("Digite a quantidade do produto: "))
-    novoUsuario = inserirUsuario()
-    # , "usuario": usuario, "produto": produto, "vendedor": vendedor}
-    newvalues = { "$set": { "nome": novoNome}, "$set": { "preco": novoPreco },
-                  "$set": { "quant_produto": novaQuantidade }}
-    print(mycol.update_one(myquery, newvalues))
-
 def deletarCompra(id):
     global mydb
     mycol = mydb.compra
-    mydict = { "id": ObjectId(id) }
-    print( mycol.delete_one(mydict))
+    mydict = { "_id": ObjectId(id) }
+    return mycol.delete_one(mydict)
+    
